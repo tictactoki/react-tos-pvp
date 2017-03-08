b0ae3dac5361f6fff4ec76740228529682bea698
Stéphane Wong <stephane.wong1@gmail.com>
2017-03-08 16:10:34 +0100

    add first and second player event

--- src/models/MainStat.js
+++ src/models/MainStat.js
@@ -12,35 +12,76 @@ export default class MainStat extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            label: this.props.firstCircles[0].label,
-            select: null,
-            firstCircles: this.props.firstCircles,
-            level: 1,
-            mainStat: {
-                str: 0,
-                con: 0,
-                int: 0,
-                spr: 0,
-                dex: 0
+            firstPlayer: {
+                label: this.props.firstCircles[0].label,
+                select: null,
+                level: 1,
+                mainStat: {
+                    str: 0,
+                    con: 0,
+                    int: 0,
+                    spr: 0,
+                    dex: 0
+                },
+                stuffId: null
             },
-            stuffId: null
+            secondPlayer: {
+                label: this.props.firstCircles[0].label,
+                select: null,
+                level: 1,
+                mainStat: {
+                    str: 0,
+                    con: 0,
+                    int: 0,
+                    spr: 0,
+                    dex: 0
+                },
+                stuffId: null
+            }
         };
         this.computeStats = this.computeStats.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleMainStat = this.handleMainStat.bind(this);
-        this.handleSelect = this.handleSelect.bind(this);
+        this.handleFirstSelect = this.handleFirstSelect.bind(this);
+        this.handleSecondSelect = this.handleSecondSelect.bind(this);
     }
 
-    handleChange(field, event) {
-        let obj = {};
-        obj[field] = parseInt(event.target.value);
-        this.setState(obj);
+    handleChange(player, field, event) {
+        if (player == 1) {
+            let fp = this.state.firstPlayer;
+            console.log(fp)
+            fp[field] = parseInt(event.target.value);
+            this.setState({firstPlayer: fp});
+        }
+        else {
+            let sp = this.state.secondPlayer;
+            sp[field] = parseInt(event.target.value);
+            this.setState({secondPlayer: sp});
+        }
     }
 
-    handleMainStat(field, event) {
-        let obj = this.state.mainStat;
-        obj[field] = parseInt(event.target.value);
-        this.setState({mainStat: obj});
+    handleMainStat(player, field, event) {
+        console.log(player,field);
+        if (player == 1) {
+            console.log("first");
+            let fp = this.state.firstPlayer;
+            let obj = fp.mainStat;
+            obj[field] = parseInt(event.target.value);
+            fp['mainStat'] = obj;
+            console.log(fp);
+            this.setState({firstPlayer: fp});
+        }
+        else {
+            console.log("second");
+            let sp = this.state.secondPlayer;
+            let obj = sp.mainStat;
+            obj[field] = parseInt(event.target.value);
+            sp['mainStat'] = obj;
+            console.log(sp);
+            this.setState({secondPlayer: sp});
+        }
+
+
     }
 
     computeStats(event) {
@@ -54,53 +95,128 @@ export default class MainStat extends React.Component {
         this.props.computeCircle(build);
     }
 
-    handleSelect(select) {
+    handleFirstSelect(select) {
         if (select != null) {
-            this.setState({label: select.label, mainStat: select.value, level: 1});
+            console.log("in select");
+            let fp = this.state.firstPlayer;
+            fp['label'] = select.label;
+            fp['mainStat']= select.value;
+            fp['level'] = 1;
+            console.log(fp);
+            this.setState({firstPlayer: fp});
+        }
+    }
+
+    handleSecondSelect(select) {
+        if (select != null) {
+            let sp = this.state.secondPlayer;
+            sp['label'] = select.label;
+            sp['mainStat']= select.value;
+            sp['level'] = 1;
+            this.setState({secondPlayer: sp});
         }
     }
 
     render() {
+        console.log(this.state.firstPlayer);
+        console.log(this.state.secondPlayer);
         return (
             <form onSubmit={this.computeStats}>
                 <div>
                     <Select
                         name="form-field-name"
-                        value={this.state.label}
+                        value={this.state.firstPlayer.label}
                         options={this.props.firstCircles}
-                        onChange={this.handleSelect}
+                        onChange={this.handleFirstSelect}
+                        className="select first-select"
                     />
-                    <div>
-                        <label>level</label>
-                        <input type="number" value={this.state.level} onChange={this.handleChange.bind(this, 'level')}/>
-                    </div>
-                    <div>
-                        <label>str</label>
-                        <input type="number" value={this.state.mainStat.str}
-                               onChange={this.handleMainStat.bind(this, 'str')}/>
-                    </div>
-                    <div>
-                        <label>con</label>
-                        <input type="text" value={this.state.mainStat.con}
-                               onChange={this.handleMainStat.bind(this, 'con')}/>
-                    </div>
-                    <div>
-                        <label>int</label>
-                        <input type="text" value={this.state.mainStat.int}
-                               onChange={this.handleMainStat.bind(this, 'int')}/>
-                    </div>
-                    <div>
-                        <label>spr</label>
-                        <input type="text" value={this.state.mainStat.spr}
-                               onChange={this.handleMainStat.bind(this, 'spr')}/>
-                    </div>
-                    <div>
-                        <label>dex</label>
-                        <input type="text" value={this.state.mainStat.dex}
-                               onChange={this.handleMainStat.bind(this, 'dex')}/>
-                    </div>
-                    <input type="submit" value="compute"/>
                 </div>
+                <div>
+                    <Select
+                        name="form-field-name"
+                        value={this.state.secondPlayer.label}
+                        options={this.props.firstCircles}
+                        onChange={this.handleSecondSelect}
+                        className="select second-select"
+                    />
+                </div>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>First Player</th>
+                    </tr>
+                    </thead>
+                    <tbody className="firstPlayer-mainStat">
+                    <tr>
+                        <td>
+                            <label>level</label>
+                            <input type="number" value={this.state.firstPlayer.level} onChange={this.handleChange.bind(this, 1,'level')}/>
+                        </td>
+                        <td>
+                            <label>str</label>
+                            <input type="number" value={this.state.firstPlayer.mainStat.str} onChange={this.handleMainStat.bind(this, 1, 'str')}/>
+                        </td>
+                        <td>
+                            <label>con</label>
+                            <input type="number" value={this.state.firstPlayer.mainStat.con} onChange={this.handleMainStat.bind(this, 1,'con')}/>
+                        </td>
+                        <td>
+                            <label>int</label>
+                            <input type="number" value={this.state.firstPlayer.mainStat.int}
+                                   onChange={this.handleMainStat.bind(this, 1, 'int')}/>
+                        </td>
+                        <td>
+                            <label>spr</label>
+                            <input type="number" value={this.state.firstPlayer.mainStat.spr} onChange={this.handleMainStat.bind(this, 1, 'spr')}/>
+                        </td>
+                        <td>
+                            <label>dex</label>
+                            <input type="number" value={this.state.firstPlayer.mainStat.dex} onChange={this.handleMainStat.bind(this, 1,'dex')}/>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Second Player</th>
+                    </tr>
+                    </thead>
+                    <tbody className="secondPlayer-mainStat">
+                    <tr>
+                        <td>
+                            <label>level</label>
+                            <input type="number" value={this.state.secondPlayer.level}
+                                   onChange={this.handleChange.bind(this, 2,'level')}/>
+                        </td>
+                        <td>
+                            <label>str</label>
+                            <input type="number" value={this.state.secondPlayer.mainStat.str}
+                                   onChange={this.handleMainStat.bind(this, 2, 'str')}/>
+                        </td>
+                        <td>
+                            <label>con</label>
+                            <input type="number" value={this.state.secondPlayer.mainStat.con}
+                                   onChange={this.handleMainStat.bind(this, 2,'con')}/>
+                        </td>
+                        <td>
+                            <label>int</label>
+                            <input type="number" value={this.state.secondPlayer.mainStat.int}
+                                   onChange={this.handleMainStat.bind(this, 2,'int')}/>
+                        </td>
+                        <td>
+                            <label>spr</label>
+                            <input type="number" value={this.state.secondPlayer.mainStat.spr}
+                                   onChange={this.handleMainStat.bind(this, 2, 'spr')}/>
+                        </td>
+                        <td>
+                            <label>dex</label>
+                            <input type="number" value={this.state.secondPlayer.mainStat.dex}
+                                   onChange={this.handleMainStat.bind(this, 2,'dex')}/>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
             </form>
         );
     }
